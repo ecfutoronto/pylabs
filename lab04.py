8902a2f5c830836b69a5409d68037eb678b24688
jwu <jwu@jwu.local>
2020-06-16 01:07:35 -0400

    timer

--- lab04.py
+++ lab04.py
@@ -1,7 +1,17 @@
 '''
 adding random comments
 '''
+from contextlib import contextmanager
+from timeit import default_timer
 
+@contextmanager
+def elapsed_timer():
+    start = default_timer()
+    elapser = lambda: default_timer() - start
+    yield lambda: elapser()
+    end = default_timer()
+    elapser = lambda: end-start
+    
 def DecimalToBinary(num): 
       
     if num > 1: 
